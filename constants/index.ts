dfc8b472555fa61112a76f8e8ba823eea6a48ae4
SimoneRodriguess <165104170+SimoneRodriguess@users.noreply.github.com>
2025-06-07 13:51:41 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 
-  import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+  import { CreateAssistantDTO, CreateWorkflowDTO } from "@vapi-ai/web/dist/api";
   import { z } from "zod";
 
 
@@ -98,6 +98,359 @@ export const mappings = {
   vercel: "vercel",
   "aws amplify": "amplify",
 };
+
+export const generator: CreateWorkflowDTO = {
+
+
+{
+  "name": "interviewprepworkflow",
+  "nodes": [
+    {
+      "name": "start_node",
+      "type": "start",
+      "metadata": {
+        "position": {
+          "x": 0,
+          "y": -70.00764694224787
+        }
+      }
+    },
+    {
+      "name": "say",
+      "type": "say",
+      "metadata": {
+        "position": {
+          "x": -99.66926769953409,
+          "y": 65.06028633453042
+        }
+      },
+      "prompt": "",
+      "exact": "Hello, {{username}}! Let's begin with your interview! For getting an interview that is customized and tailor-made according to your preferences, kindly answer a few few questions first and we'll be good to go! So, {{username}}, are you ready?"
+    },
+    {
+      "name": "role",
+      "type": "conversation",
+      "metadata": {
+        "position": {
+          "x": -110.9561074519655,
+          "y": 403.8710725568851
+        }
+      },
+      "prompt": "What role would you like to train for? ",
+      "model": {
+        "model": "gpt-4o",
+        "provider": "openai",
+        "maxTokens": 1000,
+        "temperature": 0.7
+      },
+      "variableExtractionPlan": {
+        "output": [
+          {
+            "type": "string",
+            "title": "variable",
+            "description": ""
+          }
+        ]
+      },
+      "messagePlan": {
+        "firstMessage": "role"
+      }
+    },
+    {
+      "name": "type",
+      "type": "conversation",
+      "metadata": {
+        "position": {
+          "x": -113.31018517942621,
+          "y": 627.6338427251612
+        }
+      },
+      "prompt": "Are you aiming for a technical, behavioural or mixed interview?",
+      "model": {
+        "model": "gpt-4o",
+        "provider": "openai",
+        "maxTokens": 1000,
+        "temperature": 0.7
+      },
+      "variableExtractionPlan": {
+        "output": [
+          {
+            "type": "string",
+            "title": "variable",
+            "description": ""
+          }
+        ]
+      },
+      "messagePlan": {
+        "firstMessage": ""
+      }
+    },
+    {
+      "name": "level",
+      "type": "conversation",
+      "metadata": {
+        "position": {
+          "x": -114.90205262500773,
+          "y": 843.949169220827
+        }
+      },
+      "prompt": "  Okay, I hear that, moving on, what would be your job experience?",
+      "model": {
+        "model": "gpt-4o",
+        "provider": "openai",
+        "maxTokens": 1000,
+        "temperature": 0.7
+      },
+      "variableExtractionPlan": {
+        "output": [
+          {
+            "type": "string",
+            "title": "variable",
+            "description": ""
+          }
+        ]
+      },
+      "messagePlan": {
+        "firstMessage": ""
+      }
+    },
+    {
+      "name": "amount",
+      "type": "conversation",
+      "metadata": {
+        "position": {
+          "x": -111.41947029630042,
+          "y": 1304.987931705079
+        }
+      },
+      "prompt": "How many questions, would you prefer to be asked in this interview?",
+      "model": {
+        "model": "gpt-4o",
+        "provider": "openai",
+        "maxTokens": 1000,
+        "temperature": 0.7
+      },
+      "variableExtractionPlan": {
+        "output": [
+          {
+            "type": "string",
+            "title": "variable",
+            "description": ""
+          }
+        ]
+      },
+      "messagePlan": {
+        "firstMessage": ""
+      }
+    },
+    {
+      "name": "techstack",
+      "type": "conversation",
+      "metadata": {
+        "position": {
+          "x": -109.69158415553258,
+          "y": 1084.937354161859
+        }
+      },
+      "prompt": "Okay, okay and could you list out the techstack that you're comfortable with?",
+      "model": {
+        "model": "gpt-4o",
+        "provider": "openai",
+        "maxTokens": 1000,
+        "temperature": 0.7
+      },
+      "variableExtractionPlan": {
+        "output": [
+          {
+            "type": "string",
+            "title": "variable",
+            "description": ""
+          }
+        ]
+      },
+      "messagePlan": {
+        "firstMessage": ""
+      }
+    },
+    {
+      "name": "API Request",
+      "type": "tool",
+      "metadata": {
+        "position": {
+          "x": -111.41947029630042,
+          "y": 1554.987931705079
+        }
+      },
+      "tool": {
+        "url": "https://i-ready-fivb.vercel.app/api/vapi/generate",
+        "body": {
+          "type": "object",
+          "required": [
+            "role",
+            "type",
+            "amount"
+          ],
+          "properties": {
+            "role": {
+              "type": "string",
+              "value": "{{role}}",
+              "description": ""
+            },
+            "type": {
+              "type": "string",
+              "value": "{{type}}",
+              "description": ""
+            },
+            "level": {
+              "type": "string",
+              "value": "{{level}}",
+              "description": ""
+            },
+            "amount": {
+              "type": "string",
+              "value": "{{amount}}",
+              "description": ""
+            },
+            "userid": {
+              "type": "string",
+              "value": "{{ userid }}",
+              "description": ""
+            },
+            "techstack": {
+              "type": "string",
+              "value": "{{techstack}}",
+              "description": ""
+            }
+          }
+        },
+        "type": "apiRequest",
+        "method": "POST",
+        "function": {
+          "name": "untitled_tool",
+          "parameters": {
+            "type": "object",
+            "required": [],
+            "properties": {}
+          }
+        }
+      }
+    },
+    {
+      "name": "conversation_1748536973841",
+      "type": "conversation",
+      "metadata": {
+        "position": {
+          "x": -111.41947029630042,
+          "y": 1804.987931705079
+        }
+      },
+      "prompt": "Say that the interview has been generated and thank the user for connecting with iReady!",
+      "model": {
+        "model": "gpt-4o",
+        "provider": "openai",
+        "maxTokens": 1000,
+        "temperature": 0.7
+      },
+      "messagePlan": {
+        "firstMessage": ""
+      }
+    },
+    {
+      "name": "hangup_1748537071106",
+      "type": "tool",
+      "metadata": {
+        "position": {
+          "x": -115.31784768242528,
+          "y": 2062.8484544416465
+        }
+      },
+      "tool": {
+        "type": "endCall"
+      }
+    }
+  ],
+  "edges": [
+    {
+      "from": "start_node",
+      "to": "say"
+    },
+    {
+      "from": "say",
+      "to": "role",
+      "condition": {
+        "type": "ai",
+        "prompt": "if the user said yes"
+      }
+    },
+    {
+      "from": "role",
+      "to": "type",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    },
+    {
+      "from": "type",
+      "to": "level",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    },
+    {
+      "from": "level",
+      "to": "techstack",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    },
+    {
+      "from": "techstack",
+      "to": "amount",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    },
+    {
+      "from": "amount",
+      "to": "API Request",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    },
+    {
+      "from": "API Request",
+      "to": "conversation_1748536973841",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    },
+    {
+      "from": "conversation_1748536973841",
+      "to": "hangup_1748537071106",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    }
+  ],
+  "globalPrompt": ""
+},
+
+
+    },
+
+
+  ],
+
+
+};
+
 
 export const interviewer: CreateAssistantDTO = {
   name: "Interviewer",
